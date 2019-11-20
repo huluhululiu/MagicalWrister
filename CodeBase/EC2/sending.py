78142aad812be59669899f9a57b1b2f06f76a8ab
David Yang <yang.david@wustl.edu>
2019-11-20 12:52:55 -0600

    merged serialPlotter with sending.py

--- CodeBase/EC2/sending.py
+++ CodeBase/EC2/sending.py
@@ -1,7 +1,38 @@
-import sys
 import paho.mqtt.client as mqtt #import the client1
 import time
+import enum
+import sys, serial, keyboard, re
+import numpy as np
+import time
+import threading
+import enum
 
+port = '/dev/cu.usbserial-1420'
+
+ser_bytes = [0,0,0,0,0,0,0,0]
+
+class Gesture(enum.Enum): 
+    looseFist = 0
+    tightFist = 100
+    openHand = 255
+
+def getGesture(ser_bytes):
+  averageReading = sum(ser_bytes)/len(ser_bytes)
+  #print(averageReading)
+  if(averageReading < 4000):
+    return Gesture.looseFist
+  else:
+    return Gesture.openHand
+
+def getSerBytes(ser_bytes):
+  numBytes = ser.inWaiting()
+  if(numBytes>0):
+    ser_bytes = str(ser.readline())[:-4]
+    ser_bytes = str(re.sub('([^\d,.])','', ser_bytes))
+    ser_bytes = ser_bytes.split(',')
+    ser_bytes = [float(x) for x in ser_bytes if x is not '']
+  return ser_bytes
+ 
 def on_connect(client, userdata, flags, rc):
  
     if rc == 0:
@@ -45,17 +76,27 @@ client.loop_start()        #start the loop
 while Connected != True:    #Wait for connection
     time.sleep(0.1)
 try:
+    ser = serial.Serial(port, baudrate=115200)
+    ser.flushInput()
+    current_gesture = Gesture.openHand
     while True:
-        value =input('Enter the message:')
-        client.publish("test",value)
- 
+        ser_bytes = getSerBytes(ser_bytes)
+        new_gesture = getGesture(ser_bytes)
+        if(new_gesture != current_gesture):
+        #value =input('Enter the message:')
+            client.publish("test",new_gesture.value)
+            print("Sent Gesture", new_gesture.name, "val: ", new_gesture.value)
+            current_gesture = new_gesture
+
         # number=input("enter a number")
         # client.connect(broker_address, port=port)
         # ret=client.publish("test",number)
         # print("publish?",ret)
-        time.sleep(1.5)
+        time.sleep(0.05)
+    ser.close
 except KeyboardInterrupt:
     client.disconnect()
     client.loop_stop()
-
-
+except:
+    print("Unexpected error:", sys.exc_info()[0])
+    print("Unexpected error:", sys.exc_info()[1])
--- CodeBase/pi/serialPlotter.py
+++ CodeBase/pi/serialPlotter.py
@@ -7,69 +7,52 @@ import matplotlib.animation as animation
 import time
 import threading
 import enum
-port = '/dev/cu.usbserial-1410'
-fig, ax = plt.subplots()
-x = ['A0','A1','A2','A3','A5','A14','A15','A16']
-plt.ylim(0, 3)
+
+port = '/dev/cu.usbserial-1420'
+
 ser_bytes = [0,0,0,0,0,0,0,0]
-rbarcollection = plt.bar(x, ser_bytes)
-def update(i):
-  print("here")
-  for i, b in enumerate(rbarcollection):
-    b.set_height(ser_bytes[i])
-    plt.draw()
-def graph():
-  anim = animation.FuncAnimation(fig, update, frames=100, interval=100)
-
-# thread1 = threading.Thread(target = graph)
-# thread1.start()
-
-def press(event):
-    print('press', event.key)
-    sys.stdout.flush()
-    if event.key == 'q':
-        exit()
-fig.canvas.mpl_connect('key_press_event', press)
 
 class Gesture(enum.Enum): 
-    looseFist = 1
-    tightFist = 2
-    openHand = 3
+    looseFist = 0
+    tightFist = 100
+    openHand = 255
 
-def getGesture(averageReading, currentGesture):
+def getGesture(ser_bytes):
+  averageReading = sum(ser_bytes)/len(ser_bytes)
+  # print(averageReading)
   if(averageReading < 4000):
-    if(currentGesture != Gesture.looseFist):
-      currentGesture = Gesture.looseFist
-      print(Gesture.looseFist.name)
-    #TODO: send commands related to a looseFist Gesture to IOT Devices
-
+    return Gesture.looseFist
   else:
-    #TODO: send commands related to a looseFist Gesture to IOT Devices
-    if(currentGesture != Gesture.openHand):
-      currentGesture = Gesture.openHand
-      print(Gesture.openHand.name)
+    return Gesture.openHand
 
-def getTwoFingerCurl(averageReading):
-  
-
+def getSerBytes(ser_bytes):
+  numBytes = ser.inWaiting()
+  if(numBytes>0):
+    ser_bytes = str(ser.readline())[:-4]
+    ser_bytes = str(re.sub('([^\d,.])','', ser_bytes))
+    ser_bytes = ser_bytes.split(',')
+    ser_bytes = [float(x) for x in ser_bytes if x is not '']
+  return ser_bytes
+ 
 try:
     ser = serial.Serial(port, baudrate=115200)
     ser.flushInput()
-    currentGesture = Gesture.openHand
+    current_gesture = Gesture.openHand
     while True:
-      numBytes = ser.inWaiting()
-      if(numBytes>0):
-        ser_bytes = str(ser.readline())[:-4]
-        ser_bytes = str(re.sub('([^\d,.])','', ser_bytes))
-        ser_bytes = ser_bytes.split(',')
-        ser_bytes = [float(x) for x in ser_bytes if x is not '']
-        average = sum(ser_bytes)/len(ser_bytes)
-        #print(average)
-        getGesture(average, currentGesture)
-        #print(ser_bytes)
-        if(len(ser_bytes) is not 8):
-          continue
+        ser_bytes = getSerBytes(ser_bytes)
+        new_gesture = getGesture(ser_bytes)
+        #print(new_gesture.name)
+        if(new_gesture != current_gesture):
+            #client.publish("test",new_gesture.value)
+            print("Sent Gesture", new_gesture.name, "val: ", new_gesture.value)
+            current_gesture = new_gesture
+
+        # number=input("enter a number")
+        # client.connect(broker_address, port=port)
+        # ret=client.publish("test",number)
+        # print("publish?",ret)
+        time.sleep(0.05)
     ser.close
 except:
-    print("Unexpected error:", sys.exc_info()[0])
-    print("Unexpected error:", sys.exc_info()[1])
+  print("Unexpected error:", sys.exc_info()[0])
+  print("Unexpected error:", sys.exc_info()[1])
