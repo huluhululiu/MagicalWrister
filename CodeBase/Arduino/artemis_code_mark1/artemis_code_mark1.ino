2d2bf38257b639874cfc188a9ad51a4ac18c15e9
David Yang <yang.david@wustl.edu>
2019-11-19 23:21:54 -0600

    sliding average on Arduino side

--- CodeBase/Arduino/artemis_code_mark1/artemis_code_mark1.ino
+++ CodeBase/Arduino/artemis_code_mark1/artemis_code_mark1.ino
@@ -3,24 +3,25 @@
 #define VDD 3.3
 #define SAMPLES 100
 #define NUMSENSOR 8
-#define SAMPLE_DELAY 10 //ms
-
+#define SAMPLE_DELAY 50 //ms
+#define WINDOW_SIZE 10 //size of sliding window for sliding average
 struct sensor{
   int pin;
+  double sum;
   double reading [SAMPLES];
 };
 
 int sensorPtr;
 sensor sensorArray [NUMSENSOR];
 int samplePtr;
-
+double window [NUMSENSOR][WINDOW_SIZE];
 
 void setup() {
   analogReadResolution(14);
   Serial.begin(115200);
   uint8_t analog_pins[] = {A0,A1,A2,A3,A5,A14,A15,A16};
   for (sensorPtr=0; sensorPtr<NUMSENSOR; sensorPtr++){
-    sensor sTemp = {analog_pins[sensorPtr], * new double [SAMPLES]};
+    sensor sTemp = {analog_pins[sensorPtr], 0, * new double [SAMPLES]};
     sensorArray[sensorPtr] = sTemp;
   }
   sensorPtr = 0;
@@ -32,17 +33,18 @@ void loop() {
   double reading;
   for (int i=0; i<SAMPLES; i++){
     for(int j=0; j<NUMSENSOR; j++){
-      sVolt = VDD * (analogRead(sensorArray[j].pin)/16384.0);
-      reading = ((VDD-sVolt)*RFIX)/(sVolt);
-      sensorArray[j].reading[i] = reading;
-      
-      Serial.print(reading);
-      Serial.print(',');
+//      sVolt = VDD * (analogRead(sensorArray[j].pin)/16384.0);
+//      reading = ((VDD-sVolt)*RFIX)/(sVolt);
+//      sensorArray[j].reading[i] = reading;
+      reading = analogRead(sensorArray[j].pin);
+      sensorArray[j].sum = reading + sensorArray[j].sum;
     }
-    //Serial.print(';');
-    Serial.print('\n');
-    delay(SAMPLE_DELAY);
   }
+  for (int i = 0; i < NUMSENSOR; i++){
+    Serial.print(sensorArray[i].sum / SAMPLES);
+    sensorArray[i].sum = 0;
+    Serial.print(",");
+  }
+  Serial.println();
   delay(SAMPLE_DELAY);
-  
 }
